85f31b3bff545c9e5ac4e05833ff791ceb171456
DanielBallage <danieljballage@gmail.com>
2020-06-28 15:41:26 -0400

    added variables and template literal to js

---
+++ app.js
@@ -0,0 +1,11 @@
+const adjective1 = prompt("");
+const name1 = prompt("");
+const place1 = prompt("");
+const vehicle1 = prompt("");
+const thing1 = prompt("");
+const adjective2 = prompt("");
+const thing2 = prompt("");
+const number1 = prompt("");
+const place2 = prompt("");
+
+const story = `Once there was a ${adjective1} man name ${name1}. He lived in a place called ${place1}. Every day, he drove his ${vehicle1} to the ${thing1} factory and worked all day. After work he went straight to his favorite bar, the ${adjective2} ${thing2} and drank margaritas all night. When he reached the age of ${number1}, he finally retired and moved to ${place2}, where he lived happily ever after!`
